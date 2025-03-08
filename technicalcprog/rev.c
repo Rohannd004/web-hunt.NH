a62b51901d07ff3e7edbf18636a042f9ef15b29e
Rohan04 <rohanndbrigade@gmail.com>
2025-03-08 15:43:32 +0530

    done3

---
+++ technicalcprog/rev.c
@@ -0,0 +1,26 @@
+//
+//#include <stdio.h>
+//#include <string.h>
+//#include<stdlib.h>
+//
+//void reverseString(char str[]) {
+//    int start = 0;
+//    int end = 0;
+//
+//    while(str[end] != '\0') {
+//        end++;
+//    }
+//    end--;
+//
+//    while(start < end) {
+//            char temp = str[start];
+//            str[start] = str[end];
+//            str[end] = temp;
+//
+//            start++;
+//            end--;
+//    }
+//}
+//
+//
+//
