510541f529860ad49ab080c365453cbfaaa2f3a6
Rohan04 <rohanndbrigade@gmail.com>
2025-03-08 09:03:55 +0530

    done

---
+++ technicalcprog/conversion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+char alp() {
+   int x;
+   printf("Enter the integer: ");
+   scanf("%d", &x);
+  return x;
+int main() {
+   printf("%c", alp()); // Call the alp function and print the result as a character
+   return 0;
+}
